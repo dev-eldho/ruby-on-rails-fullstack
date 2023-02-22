c4fd66d3be79a7735845c0944ae584ce4f183495
eldho.thampi <eldho.thampi@innovaturelabs.org>
2023-02-22 14:00:53 +0530

    note app crud feature added

--- db/migrate/20230220060910_create_notes.rb
+++ db/migrate/20230220060910_create_notes.rb
@@ -1,6 +1,6 @@
-class CreateNote < ActiveRecord::Migration[7.0]
+class CreateNotes < ActiveRecord::Migration[7.0]
   def change
-    create_table :note,:primary_key =>:note_id do |t|
+    create_table :notes,:primary_key =>:note_id do |t|
       t.string :title,null:false
       t.string :description,null:false
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_20_060910) do
-  create_table "note", primary_key: "note_id", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "notes", primary_key: "note_id", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
     t.datetime "created_at", null: false
