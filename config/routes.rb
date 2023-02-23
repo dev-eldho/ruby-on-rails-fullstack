c272d2c158dbce14da891ba8cc738127ec3480fa
eldho.thampi <eldho.thampi@innovaturelabs.org>
2023-02-23 10:57:15 +0530

    user authentication module added using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :notes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
  root "notes#index"
   resources:notes
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
 end
